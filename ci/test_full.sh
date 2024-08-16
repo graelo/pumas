ca7aadd96506f778bbe67e8964ce9d0a4eb76ea3
graelo <graelo@graelo.cc>
2024-08-16 02:20:29 +0200

    chore(ci): upgrade actions

--- ci/test_full.sh
+++ ci/test_full.sh
@@ -3,7 +3,7 @@
 set -e
 
 CRATE=pumas
-MSRV=1.64
+MSRV=1.74
 
 get_rust_version() {
   local array=($(rustc --version));
@@ -28,7 +28,7 @@ if ! check_version $MSRV ; then
 fi
 
 FEATURES=()
-# check_version 1.64 && FEATURES+=(libm)
+# check_version 1.74 && FEATURES+=(libm)
 echo "Testing supported features: ${FEATURES[*]}"
 
 set -x
